85782dd02f224e9badf5417cc3c4d078970b47fc
Michael Quinn <mquinn@gloto.com>
2013-07-23 17:35:45 -0400

    Update git-server role
    * Sets up LDAP authentication
    * Installs git tools
    * Mounts EBS volume

--- roles/git-server.rb
+++ roles/git-server.rb
@@ -2,21 +2,22 @@ name "git-server"
 description "Hosts all of Gloto's Git repositories"
 default_attributes()
 override_attributes(
-  :set_fqdn => "source.gloto.com"
-=begin
+  :set_fqdn => "source.gloto.com",
   :ebs => {
     :creds => {
-      :databag => "AWS",
-      :item => "chef-server-keys",
-      :encrypted => false
+      :databag => "passwords",
+      :item => "AWS",
+      :encrypted => true
     },
     :volumes => {
       "/data" => { :device => "/dev/xvdf" }
     }
   }
-=end
 )
 run_list(
   "role[base-ubuntu]",
-  "recipe[hostname]"
+  "recipe[hostname]",
+  "recipe[gloto-chef-cookbook::ldap-client]",
+  "recipe[gloto-chef-cookbook::git-host]",
+  "recipe[ebs]"
 )
